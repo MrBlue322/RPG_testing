61bc27895b62a828a25181ebccea8edfb582bbb2
Dawson <Dawson.barthelemy@gmail.com>
2023-09-30 21:35:34 -0500

    added some comments

--- Game/Player/Player.gd
+++ Game/Player/Player.gd
@@ -6,10 +6,11 @@ const JUMP_VELOCITY = -1020.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var v_M = 1
+
 
 	
-	
+#reverse velocity if you hit a wall
+var v_M = 1
 func _hit_wall():
 	if is_on_wall():
 		v_M = -1
@@ -18,18 +19,18 @@ func _hit_wall():
 		
 	
 		
-
+#movement
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta 
 		
 	# Handle Jump.
+	#>project>project settings>Input Map to accses grouped keyboard layouts
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
  
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED * v_M
